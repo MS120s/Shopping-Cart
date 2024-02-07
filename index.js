423e0ab0631c894f8f879dfc20a70362ff468189
MS120s <umarbalogunsulaiman@gmail.com>
2024-02-07 19:50:31 +0100

    w
    q

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-const containerEl = document.getElementById("container")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-let img = ["images/cat.png"]
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-containerEl.innerHTML = `<img src="${img[0]}">`
+const appSettings = {
+    databaseURL: "https://realtime-database-dd610-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "ShoppingList")
+const booksListInDB = ref(database, "Books")
+
+const buttonEl = document.getElementById("add-button")
+const inputEl = document.getElementById("input-field")
+const shopListEl = document.getElementById("shopping-list")
+
+// refactor codes
+function cler(a) {
+     return a.value = ""
+}
+
+function appendList(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    shopListEl.append(newEl)
+
+    newEl.addEventListener("click", function(){
+        let exactLocation = ref(database, `ShoppingList/${itemID}`)
+        remove(exactLocation)
+    })
+    
+}
+
+function clearData(data){
+    data.innerHTML = ""
+}
+
+
+buttonEl.addEventListener("click", function(){
+    let inputValue = inputEl.value
+    push(shoppingListInDB, inputValue)
+    cler(inputEl)
+
+})
+
+onValue(shoppingListInDB, function(snapshot){
+    if (snapshot.exists()){
+        let shopListArray = Object.entries(snapshot.val())
+        clearData(shopListEl)
+
+        for (let i = 0; i < shopListArray.length; i++) {
+            let shopList = shopListArray[i]
+
+            let shopListID = shopList[0]
+            let shopListValues = shopList[1]
+
+            appendList(shopList)
+    }
+    } else{
+        shopListEl.innerHTML = "No items here... yet"
+    }
+    
+    
+
+})  
---
+++ practice.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: https://playground-f0ad6-default-rtdb.europe-west1.firebasedatabase.app/
+}
